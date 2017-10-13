d54e9071a6e61d04d48311e7184fe4dd36169ab4
Andrew Jones <andrew.jones@audiogum.com>
2017-10-13 11:17:47 +0100

    Update dependencies to latest versions

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/Raynes/least"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [clj-http "0.7.2"]
-                 [cheshire "5.1.1"]])
+  :dependencies [[org.clojure/clojure "1.9.0-beta2"]
+                 [clj-http "3.7.0"]
+                 [cheshire "5.8.0"]])
